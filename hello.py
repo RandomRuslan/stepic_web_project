4e5ac9782d8c416c961564e871b01837ffd01455
RandomRuslan <wrkrus@gmail.com>
2017-07-14 17:58:47 +0300

    FirstFB

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-def app(env, resp):
+def application(env, resp):
 	status = "200 OK"
 	headers = [("Content-type","text/plain")]
-	#resp(status, headers)
+	resp(status, headers)
 	return env["QUERY_STRING"].replace("&", "\n")
 	
 	
-print(app({"QUERY_STRING":"a=1&b=2&b=3"}, 0))
+#print(application({"QUERY_STRING":"a=1&b=2&b=3"}, 0))
