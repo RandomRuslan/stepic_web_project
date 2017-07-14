08b125143e2233c4a7fdf6d4516ee5512f2329f0
RandomRuslan <wrkrus@gmail.com>
2017-07-14 20:46:22 +0300

    ThirdFB

--- hello.py
+++ hello.py
@@ -1,9 +1,6 @@
-bind = "0.0.0.0:8080"
+#bind = "0.0.0.0:8080"
 def application(env, resp):
 	status = "200 OK"
 	headers = [("Content-type","text/plain")]
 	resp(status, headers)
 	return env["QUERY_STRING"].replace("&", "\n")
-	
-	
-#print(application({"QUERY_STRING":"a=1&b=2&b=3"}, 0))
--- init.sh
+++ init.sh
@@ -3,7 +3,8 @@ sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo rm /etc/gunicorn.d/test
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo ln -s /home/box/etc/hello.py    /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo rm /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/hello.py    /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
